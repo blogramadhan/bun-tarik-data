6ce2ed21a02a5d573fa20e36080eabf6cddc1027
Kurnia Ramadhan <kramadhan@gmail.com>
2025-09-19 08:40:31 +0700

    update code

--- src/tesWA.ts
+++ src/tesWA.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-await axios.post('http://localhost:3000/send-message', {
+await axios.post('http://localhost:8788/send-message', {
   number: '62811577280',
   message: 'Pesan dikirim dari file lain!'
 });
